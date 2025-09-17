1495e9b249f0be7f1fd90aeec5ba2d86a2488125
SthobileMkhabela <st10448774@imconnect.edu.za>
2025-09-17 20:30:08 +0200

    Fourth Commit
    Added a list of claims reviewed by the Academic Manager

--- CMCS_App/Controllers/AcademicManagerController.cs
+++ CMCS_App/Controllers/AcademicManagerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CMCS_App.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CMCS_App.Controllers
 {
@@ -6,7 +7,31 @@ namespace CMCS_App.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var claimsList = new List<Claim>
+    {
+        new Claim
+        {
+            ClaimID = 201,
+            Lecturer = new Lecturer { LecturerID = 1, FullName = "Dr. Smith" },
+            HoursWorked = 8,
+            Month = "August",
+            TotalAmount = 4000,
+            Status = "Approved by Coordinator",
+            SubmissionDate = DateTime.Now.AddDays(-3)
+        },
+        new Claim
+        {
+            ClaimID = 202,
+            Lecturer = new Lecturer { LecturerID = 2, FullName = "Prof. Johnson" },
+            HoursWorked = 10,
+            Month = "September",
+            TotalAmount = 6000,
+            Status = "Approved by Manager",
+            SubmissionDate = DateTime.Now.AddDays(-5)
+        }
+    };
+
+            return View(claimsList);
         }
     }
 }
